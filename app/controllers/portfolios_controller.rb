78d5692cf499e2daa623c3e9798f015a49fef25a
Sadam Hussain <sadam.hussain@codingcops.com>
2022-11-09 19:03:28 +0500

    Implemented delete Item functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -31,4 +31,11 @@ class PortfoliosController < ApplicationController
     def show
     	@portfolio_item=Portfolio.find(params[:id])
     end
+    def destroy
+    	@portfolio_item=Portfolio.find(params[:id])
+	    @portfolio_item.destroy
+	    respond_to do |format|
+	      format.html { redirect_to portfolios_url, notice: "Record was removed successfully." }
+	    end
+    end
 end
