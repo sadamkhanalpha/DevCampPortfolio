d91e4c0980c49f30b382b56beec1cf60e0b99185
Sadam Hussain <sadam.hussain@codingcops.com>
2022-11-10 20:19:26 +0500

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
 10.times do |blog|
   Blog.create!(
   	title: "My Blog Post #{blog}",
@@ -6,6 +11,7 @@
   	Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
   	 consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical 
   	 literature, discovered the undoubtable source.",
+     topic_id: Topic.last.id
   	)
 end
 
@@ -20,7 +26,7 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
   	title: "Portfolio title: #{portfolio_item}",
   	subtitle: "My great service",
@@ -31,6 +37,19 @@ puts "5 skills created"
   	 literature, discovered the undoubtable source.",
   	main_image: "https://via.placeholder.com/600x400",
   	thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece 
+    of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a 
+    Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
+     consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical 
+     literature, discovered the undoubtable source.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
   )
 end
 puts "9 portfolio items created"
